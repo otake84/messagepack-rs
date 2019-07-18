5122a3bddd838dab300237a752e4386e42479b4b
Shogo Otake <shogo.otake@gmail.com>
2019-07-18 12:52:22 +0900

    Add Nil to Value

--- src/value.rs
+++ src/value.rs
@@ -1,8 +1,19 @@
-use std::convert::From;
+use std::convert::{From, Into};
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Value {
+    Nil,
     Bool(bool),
+}
+
+impl<T: Into<Value>> From<Option<T>> for Value {
+    fn from(value: Option<T>) -> Self {
+        if let Some(value) = value {
+            value.into()
+        } else {
+            Value::Nil
+        }
+    }
 }
 
 impl From<bool> for Value {
