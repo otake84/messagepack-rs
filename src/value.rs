5a8127c120c221fbcf6670b22b427701780c6cac
Shogo Otake <shogo.otake@gmail.com>
2019-07-25 15:53:16 +0900

    Implement deserialize for Marker::Int16

--- src/value.rs
+++ src/value.rs
@@ -411,6 +411,7 @@ impl Value {
             Marker::UInt32 => Ok(Value::UInt32(buf_reader.read_u32::<BigEndian>().or(Err(DeserializeError::InvalidValue))?)),
             Marker::UInt64 => Ok(Value::UInt64(buf_reader.read_u64::<BigEndian>().or(Err(DeserializeError::InvalidValue))?)),
             Marker::Int8 => Ok(Value::Int8(buf_reader.read_i8().or(Err(DeserializeError::InvalidValue))?)),
+            Marker::Int16 => Ok(Value::Int16(buf_reader.read_i16::<BigEndian>().or(Err(DeserializeError::InvalidValue))?)),
             _ => unimplemented!(),
         }
     }
