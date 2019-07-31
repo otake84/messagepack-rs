02f9702b35704e0748d7b491fafcb3b34839e3d8
Shogo Otake <shogo.otake@gmail.com>
2019-07-31 17:32:58 +0900

    Add value::stream::serializer

---
+++ src/value/stream/serializer.rs
@@ -0,0 +1,22 @@
+use crate::value::{SerializeError, Value};
+use std::io::{BufWriter, Error, Write};
+
+pub struct Serializer<T: Write>(BufWriter<T>);
+
+impl<W: Write> Serializer<W> {
+    pub fn new(w: W) -> Self {
+        Serializer(BufWriter::new(w))
+    }
+
+    pub fn serialize(&mut self, value: Value) -> Result<usize, SerializeError> {
+        self.0.write(&value.serialize()?).or(Err(SerializeError::FailedToWrite))
+    }
+
+    pub fn get_ref(&self) -> &W {
+        self.0.get_ref()
+    }
+
+    pub fn flush(&mut self) -> Result<(), Error> {
+        self.0.flush()
+    }
+}
