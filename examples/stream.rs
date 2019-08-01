efa9af52d5957e0e8fe145287056d0eb1325c87d
Shogo Otake <shogo.otake@gmail.com>
2019-08-01 19:22:35 +0900

    Add stream to examples

---
+++ examples/stream.rs
@@ -0,0 +1,20 @@
+use messagepack_rs::stream::serializer::Serializer;
+use messagepack_rs::value::Value;
+use std::io::{BufReader, Cursor};
+
+fn main() {
+    let buf = Vec::new();
+    let mut stream_serializer = Serializer::new(buf);
+    stream_serializer.serialize(Value::Nil).unwrap();
+    stream_serializer.serialize(Value::from(true)).unwrap();
+    stream_serializer.serialize(Value::from(false)).unwrap();
+    stream_serializer.serialize(Value::from("test")).unwrap();
+    stream_serializer.serialize(Value::Nil).unwrap();
+    stream_serializer.flush().unwrap();
+    println!("{:?}", stream_serializer.get_ref());
+
+    let buf_reader = BufReader::new(Cursor::new(stream_serializer.get_ref()));
+    let stream_deserializer = messagepack_rs::stream::deserializer::Deserializer::new(buf_reader);
+    let result = stream_deserializer.deserialize::<Value>(|value, position| println!("value: {:?}, positoin: {}", value, position));
+    println!("{:?}", result);
+}
