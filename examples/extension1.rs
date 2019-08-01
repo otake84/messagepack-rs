26bff31985701a859eab04adf2a98565c6df537f
Shogo Otake <shogo.otake@gmail.com>
2019-08-01 19:37:43 +0900

    Add extension1 to examples

--- examples/extension1.rs
+++ examples/extension1.rs
@@ -3,12 +3,28 @@ use messagepack_rs::serializable::Serializable;
 use messagepack_rs::value::Value;
 use std::io::{BufReader, Cursor};
 
+#[derive(Clone, Debug, PartialEq)]
+struct Rgba {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
+
+impl From<Rgba> for Value {
+    fn from(value: Rgba) -> Self {
+        Value::Extension(0, vec![value.r, value.g, value.b, value.a])
+    }
+}
+
 fn main() {
-    let nil: Option<u8> = None;
-    let value = Value::from(vec![Value::from(123u8), Value::from("test"), Value::from(true), Value::Nil, Value::from(nil)]);
+    let rgba = Rgba { r: 5, g: 10, b: 15, a: 20 };
+    let value = Value::from(rgba);
     println!("{:?}", value);
+
     let serialized_value = value.serialize().unwrap();
     println!("{:?}", serialized_value);
+
     let deserialized_value = Value::deserialize(&mut BufReader::new(Cursor::new(serialized_value))).unwrap();
     println!("{:?}", deserialized_value);
 }
