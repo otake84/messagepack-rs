7c0dc2b070b992152ad8ced2d2cdfd4408f93ccb
Shogo Otake <shogo.otake@gmail.com>
2019-08-16 03:58:33 +0900

    Fix "range patterns are deprecated" warnings

--- src/marker/from.rs
+++ src/marker/from.rs
@@ -3,10 +3,10 @@ use super::Marker;
 impl From<u8> for Marker {
     fn from(n: u8) -> Marker {
         match n {
-            0x00...0x7f => Marker::PositiveFixInt(n),
-            0x80...0x8f => Marker::FixMap(n & 0x0f),
-            0x90...0x9f => Marker::FixArray(n & 0x0f),
-            0xa0...0xbf => Marker::FixStr(n & 0x1f),
+            0x00..=0x7f => Marker::PositiveFixInt(n),
+            0x80..=0x8f => Marker::FixMap(n & 0x0f),
+            0x90..=0x9f => Marker::FixArray(n & 0x0f),
+            0xa0..=0xbf => Marker::FixStr(n & 0x1f),
             0xc0 => Marker::Nil,
             0xc1 => Marker::Reserved,
             0xc2 => Marker::False,
@@ -39,7 +39,7 @@ impl From<u8> for Marker {
             0xdd => Marker::Array32,
             0xde => Marker::Map16,
             0xdf => Marker::Map32,
-            0xe0...0xff => Marker::NegativeFixInt(n as i8),
+            0xe0..=0xff => Marker::NegativeFixInt(n as i8),
         }
     }
 }
